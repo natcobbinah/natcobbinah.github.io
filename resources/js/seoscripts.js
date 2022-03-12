a87db536ff2279edc773f7e0592730825cd05a95
natcobbinah <baby.hugo@yahoo.com>
2022-03-12 14:39:29 +0100

    error in seo jsonLD corrected

--- resources/js/seoscripts.js
+++ resources/js/seoscripts.js
@@ -6,60 +6,13 @@ var seoObject = [
         "author": {
             "@type": "Person",
             "givenName": "Nathaniel",
-            "familyName": "Cobbinah",
+            "familyName": "Cobbinah"
         },
         "blogPost": "I've had the taste of what it feels like to be a front-end, back-end, or combining both of these stack disciplines in my development as a professional developer.My plan to staying adaptable in the computing industry has been to have the requisite knowledge of best practices, tools and processes regarded by the industry to be optimal in building better systems that achieve business goals for individuals or corporate organizations, with top priority on establishing positive connections with clients. To sum it all, the end in sight is not only about moi-même seulement, but to making positive contributions in any environment of my presence."
-    },
-    {
-        "@context": "https://schema.org",
-        "@type": "SoftwareApplication ",
-        "name": "BrailleCode Generator",
-        "operatingSystem": ["WINDOWS", "OSX"],
-        "applicationCategory": ["DeveloperApplication", "EducationalApplication"],
-        "author": {
-            "@type": "Person",
-            "givenName": "Nathaniel",
-            "familyName": "Cobbinah",
-        }
-    },
-    {
-        "@context": "https://schema.org",
-        "@type": "SoftwareApplication ",
-        "name": "DirectionAL Drilling Calculator",
-        "operatingSystem": ["WINDOWS", "OSX"],
-        "applicationCategory": ["DeveloperApplication", "EducationalApplication"],
-        "author": {
-            "@type": "Person",
-            "givenName": "Nathaniel",
-            "familyName": "Cobbinah",
-        }
-    },
-    {
-        "@context": "https://schema.org",
-        "@type": "SoftwareApplication ",
-        "name": "TextWriter Demo",
-        "operatingSystem": ["WINDOWS", "OSX"],
-        "applicationCategory": ["DeveloperApplication", "EducationalApplication", "UtilitiesApplication"],
-        "author": {
-            "@type": "Person",
-            "givenName": "Nathaniel",
-            "familyName": "Cobbinah",
-        }
-    },
-    {
-        "@context": "https://schema.org",
-        "@type": "SoftwareApplication ",
-        "name": "Youtube Tutorials",
-        "applicationCategory": "EducationalApplication",
-        "author": {
-            "@type": "Person",
-            "givenName": "Nathaniel",
-            "familyName": "Cobbinah",
-        }
     }
 ];
 
-seoObject.forEach(structuredTextData => {
+seoObject.map(structuredTextData => {
     const script = document.createElement('script');
     script.setAttribute('type', 'application/ld+json');
     script.textContent = structuredTextData;
