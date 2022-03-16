dfee8dafd16cce3992fd5f503143ff555dbc0648
natcobbinah <baby.hugo@yahoo.com>
2022-03-16 23:20:45 +0100

    toggle fxntionality separated to module

---
+++ resources/modules/dom_modules.js
@@ -0,0 +1,11 @@
+const toggleHiddenDescription = (domElement,changeClickedBtnIcon) => {
+    if (domElement.style.display === 'block') {
+        domElement.style.display = 'none';
+        changeClickedBtnIcon.innerHTML = '<i class="fa-solid fa-circle-plus"></i>';
+    } else {
+        domElement.style.display = 'block';
+        changeClickedBtnIcon.innerHTML = '<i class="fa-solid fa-circle-minus"></i>';
+    }
+}
+
+export {toggleHiddenDescription};
