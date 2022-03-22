d547384affe7bd958d9f94b576455b0cd126659c
natcobbinah <baby.hugo@yahoo.com>
2022-03-22 18:45:26 +0100

    demo projects updated

--- resources/js/pgscript.js
+++ resources/js/pgscript.js
@@ -3,7 +3,7 @@ import { toggleHiddenDescription } from '../modules/dom_modules.js';
 //icon buttons 
 let seeMoreofBrailleBtn = document.querySelector('.brailleAppInfo');
 let seeMoreofDrillBtn = document.querySelector('.drillingAppInfo');
-let seeMoreofPayrollBtn = document.querySelector('.payrollAppInfo');
+let seeMoreofMorseCodeBtn = document.querySelector('.morseCodeAppInfo');
 let seeMoreofYoutubeBtn = document.querySelector('.youtubeAppInfo');
 let seeMoreofRestBtn = document.querySelector('.restAppInfo');
 let seeMoreofWriterBtn = document.querySelector('.textWriterAppInfo');
@@ -11,15 +11,15 @@ let seeMoreofWriterBtn = document.querySelector('.textWriterAppInfo');
 //paragraph descriptions
 let braille_description = document.getElementById('brailleDescription');
 let drill_description = document.getElementById('drillDescription');
-let payroll_description = document.getElementById('payrollDescription');
+let morseCode_description = document.getElementById('morseCodeDescription');
 let youtube_description = document.getElementById('youtubeDescription');
 let rest_description = document.getElementById('restDescription');
 let textWriter_description = document.getElementById('textWriterDescription');
 
-let allBtnsToBeClicked = [seeMoreofBrailleBtn, seeMoreofDrillBtn, seeMoreofPayrollBtn, seeMoreofYoutubeBtn,
+let allBtnsToBeClicked = [seeMoreofBrailleBtn, seeMoreofDrillBtn, seeMoreofMorseCodeBtn, seeMoreofYoutubeBtn,
     seeMoreofRestBtn, seeMoreofWriterBtn];
 
-let allDescriptionsToDisplay = [braille_description, drill_description, payroll_description, youtube_description,
+let allDescriptionsToDisplay = [braille_description, drill_description, morseCode_description, youtube_description,
     rest_description, textWriter_description];
 
 //note buttonFxntionality and its description in the subsequent array matches 
