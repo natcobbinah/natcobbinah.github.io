d64bcbe27d4ab7a783b3a07cf52b690a97a21e34
natcobbinah <baby.hugo@yahoo.com>
2022-04-09 20:13:27 +0200

    yelpBusiness Demo added to projects

--- resources/js/pgscript.js
+++ resources/js/pgscript.js
@@ -8,6 +8,7 @@ let seeMoreofYoutubeBtn = document.querySelector('.youtubeAppInfo');
 let seeMoreofRestBtn = document.querySelector('.restAppInfo');
 let seeMoreofWriterBtn = document.querySelector('.textWriterAppInfo');
 let seeMoreofHackerNewsBtn = document.querySelector('.hackerNewsAppInfo');
+let seeMoreofYelpBusineesAppBtn = document.querySelector('.YelpBusinessAppInfo');
 
 //paragraph descriptions
 let braille_description = document.getElementById('brailleDescription');
@@ -17,12 +18,13 @@ let youtube_description = document.getElementById('youtubeDescription');
 let rest_description = document.getElementById('restDescription');
 let textWriter_description = document.getElementById('textWriterDescription');
 let hackerNews_description = document.getElementById('hackerNewsAppDescription');
+let yelpBusiness_description = document.getElementById('yelpBusinessAppDescription');
 
 let allBtnsToBeClicked = [seeMoreofBrailleBtn, seeMoreofDrillBtn, seeMoreofMorseCodeBtn, seeMoreofYoutubeBtn,
-    seeMoreofRestBtn, seeMoreofWriterBtn, seeMoreofHackerNewsBtn];
+    seeMoreofRestBtn, seeMoreofWriterBtn, seeMoreofHackerNewsBtn, seeMoreofYelpBusineesAppBtn];
 
 let allDescriptionsToDisplay = [braille_description, drill_description, morseCode_description, youtube_description,
-    rest_description, textWriter_description, hackerNews_description];
+    rest_description, textWriter_description, hackerNews_description, yelpBusiness_description];
 
 //note buttonFxntionality and its description in the subsequent array matches 
 //for the loop fxtionality to work as exepcted
