627503a74354f9f62938f35c54c7309d9e187262
natcobbinah <baby.hugo@yahoo.com>
2023-01-03 16:08:06 +0100

    animations added to techstack list

--- resources/js/showHideTextScript.js
+++ resources/js/showHideTextScript.js
@@ -1,9 +1,11 @@
 const toggleHiddenDescription = (domElement,changeClickedBtnIcon) => {
     if (domElement.style.display === 'block') {
         domElement.style.display = 'none';
+        domElement.style.transition = "display 1.5s ease-in 1.5s";
         changeClickedBtnIcon.innerHTML = '<i class="fa-solid fa-angle-right"></i>';
     } else {
         domElement.style.display = 'block';
+        domElement.style.transition = "display 1.5s ease-in 1.5s";
         changeClickedBtnIcon.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
     }
 }
