73fd33234b6e8bd72c8e1f0766b3fb53b23f09a1
natcobbinah <baby.hugo@yahoo.com>
2023-03-20 23:15:38 +0100

    url path changed to host path

--- httpEndpoints/constants.js
+++ httpEndpoints/constants.js
@@ -1,4 +1,5 @@
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://shy-lime-dibbler-garb.cyclic.app:3000';
+//const baseUrl = 'http://localhost:3000';
 const emailUrl = 'api/sendMail';
 
 export {baseUrl, emailUrl};
