0f9731d080c492d07564b10131d69e540992b950
natcobbinah <baby.hugo@yahoo.com>
2023-03-22 21:16:50 +0100

    baseUrl changed to server

--- httpEndpoints/axiosInstance.js
+++ httpEndpoints/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//baseURL: "http://localhost:5478",
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5478",
+    baseURL: "https://shy-lime-dibbler-garb.cyclic.app:5478"
   });
   
   export default axiosInstance;
