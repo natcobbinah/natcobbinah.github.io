635274a9418608a470b705545fe71bdfbc1807db
natcobbinah <baby.hugo@yahoo.com>
2023-03-22 21:27:07 +0100

    port changed to default host service serverPort

--- httpEndpoints/axiosInstance.js
+++ httpEndpoints/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 //baseURL: "http://localhost:5478",
+// "https://shy-lime-dibbler-garb.cyclic.app:5478"
 
 const axiosInstance = axios.create({
-    baseURL: "https://shy-lime-dibbler-garb.cyclic.app:5478"
-  });
-  
-  export default axiosInstance;
-  
+  baseURL: "http://localhost:3000"
+});
+
+export default axiosInstance;
